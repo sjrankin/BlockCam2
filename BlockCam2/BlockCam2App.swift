75a95b5acb61f769ec5a7f6f2aa2564b604f4794
Stuart Rankin <sjrankin@gmail.com>
2021-04-13 13:36:39 +0900

    Initial Commit

---
+++ BlockCam2/BlockCam2App.swift
@@ -0,0 +1,17 @@
+//
+//  BlockCam2App.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BlockCam2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
