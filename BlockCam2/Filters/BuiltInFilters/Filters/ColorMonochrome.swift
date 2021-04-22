a14f0fee7da315305c5894a2295d2163219f3ed7
Stuart Rankin <sjrankin@gmail.com>
2021-04-22 13:05:29 +0900

    Create ColorMonochrome.swift
    Added color monochrome filter.

---
+++ BlockCam2/Filters/BuiltInFilters/Filters/ColorMonochrome.swift
@@ -0,0 +1,52 @@
+//
+//  ColorMonochrome.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/22/21.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import AVFoundation
+import CoreImage
+import CoreServices
+import AVKit
+import Photos
+import MobileCoreServices
+import CoreMotion
+import CoreMedia
+import CoreVideo
+import CoreImage.CIFilterBuiltins
+
+class ColorMonochrome: BuiltInFilterProtocol
+{
+    static var FilterType: BuiltInFilters = .ColorMonochrome
+    
+    static var Name: String = "Color Monochrome"
+    
+    func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
+                   _ ColorSpace: CGColorSpace) -> CVPixelBuffer
+    {
+        let SourceImage = CIImage(cvImageBuffer: Buffer)
+        let Adjust = CIFilter.colorMonochrome()
+        Adjust.color = CIColor.blue
+        Adjust.inputImage = SourceImage
+        if let Adjusted = Adjust.outputImage
+        {
+            var PixBuf: CVPixelBuffer? = nil
+            CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool, &PixBuf)
+            guard let OutPixBuf = PixBuf else
+            {
+                fatalError("Allocation failure in \(#function)")
+            }
+            CIContext().render(Adjusted, to: OutPixBuf, bounds: SourceImage.extent,
+                               colorSpace: ColorSpace)
+            return OutPixBuf
+        }
+        else
+        {
+            return Buffer
+        }
+    }
+}
