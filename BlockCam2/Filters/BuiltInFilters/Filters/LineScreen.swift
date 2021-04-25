1b5ebef8d02c22a1a6d780d8edf27487c79286ca
Stuart Rankin <sjrankin@gmail.com>
2021-04-25 21:09:14 +0900

    Update LineScreen.swift
    Removed unused code.

--- BlockCam2/Filters/BuiltInFilters/Filters/LineScreen.swift
+++ BlockCam2/Filters/BuiltInFilters/Filters/LineScreen.swift
@@ -26,32 +26,6 @@ class LineScreen: BuiltInFilterProtocol
     static var Name: String = "Line Screen"
     
     func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
-                   _ ColorSpace: CGColorSpace) -> CVPixelBuffer
-    {
-        let SourceImage = CIImage(cvImageBuffer: Buffer)
-        let Adjust = CIFilter.lineScreen()
-        Adjust.angle = 0.0
-        Adjust.center = CGPoint(x: SourceImage.extent.width / 2.0, y: SourceImage.extent.height / 2.0)
-        Adjust.inputImage = SourceImage
-        if let Adjusted = Adjust.outputImage
-        {
-            var PixBuf: CVPixelBuffer? = nil
-            CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool, &PixBuf)
-            guard let OutPixBuf = PixBuf else
-            {
-                fatalError("Allocation failure in \(#function)")
-            }
-            CIContext().render(Adjusted, to: OutPixBuf, bounds: SourceImage.extent,
-                               colorSpace: ColorSpace)
-            return OutPixBuf
-        }
-        else
-        {
-            return Buffer
-        }
-    }
-    
-    func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
                    _ ColorSpace: CGColorSpace, Options: [FilterOptions: Any]) -> CVPixelBuffer
     {
         let SourceImage = CIImage(cvImageBuffer: Buffer)
