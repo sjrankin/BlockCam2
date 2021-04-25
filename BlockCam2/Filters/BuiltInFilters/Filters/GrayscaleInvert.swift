4767882a0801fb7e809bb1adab442308e9d1eb14
Stuart Rankin <sjrankin@gmail.com>
2021-04-25 21:08:58 +0900

    Update GrayscaleInvert.swift
    Removed unused code.

--- BlockCam2/Filters/BuiltInFilters/Filters/GrayscaleInvert.swift
+++ BlockCam2/Filters/BuiltInFilters/Filters/GrayscaleInvert.swift
@@ -26,32 +26,6 @@ class GrayscaleInvert: BuiltInFilterProtocol
     static var Name: String = "Grayscale Invert"
     
     func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
-                   _ ColorSpace: CGColorSpace) -> CVPixelBuffer
-    {
-        let Gray = MinimumComponent()
-        let GrayBuffer = Gray.RunFilter(Buffer, BufferPool, ColorSpace)
-        let SourceImage = CIImage(cvImageBuffer: GrayBuffer)
-        let Adjust = CIFilter.colorInvert()
-        Adjust.inputImage = SourceImage
-        if let Adjusted = Adjust.outputImage
-        {
-            var PixBuf: CVPixelBuffer? = nil
-            CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool, &PixBuf)
-            guard let OutPixBuf = PixBuf else
-            {
-                fatalError("Allocation failure in \(#function)")
-            }
-            CIContext().render(Adjusted, to: OutPixBuf, bounds: SourceImage.extent,
-                               colorSpace: ColorSpace)
-            return OutPixBuf
-        }
-        else
-        {
-            return Buffer
-        }
-    }
-    
-    func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
                    _ ColorSpace: CGColorSpace, Options: [FilterOptions: Any]) -> CVPixelBuffer
     {
         var GrayFilter: BuiltInFilterProtocol? = nil
