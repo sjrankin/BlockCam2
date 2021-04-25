404dafdd4aa5f6bfbf736474c94a5d64bd201c7d
Stuart Rankin <sjrankin@gmail.com>
2021-04-25 21:07:13 +0900

    Update GroupButton.swift
    Updated view and now using in FilterView.

--- BlockCam2/SubViews/GroupButton.swift
+++ BlockCam2/SubViews/GroupButton.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct GroupButton: View
 {
     @State var Name: String
-    @State var IsSelected: Bool
-    @State var SelectedButton: String
+    @Binding var SelectedButton: String
+    var SelectedBorderColor: Color = Color(UIColor.link)
     
     var body: some View
     {
@@ -24,17 +24,19 @@ struct GroupButton: View
                 .font(.custom("Avenir-Black", size: 24.0))
                 .padding(.horizontal, 5)
                 .padding(.vertical, 10)
+                .shadow(radius: 5)
         }
         .background(
             ZStack
             {
                 RoundedRectangle(cornerRadius: 10.0)
-                    .stroke(Color.white,
-                            lineWidth: 5)
-                    .shadow(radius: 5)
+                    .stroke(SelectedButton == Name ? SelectedBorderColor : Color.white,
+                            style: StrokeStyle(
+                                lineWidth: SelectedButton == Name ? 10 : 5,
+                                dash: SelectedButton == Name ? [5, 5] : []
+                            ))
                 RoundedRectangle(cornerRadius: 10.0)
                     .fill(Color(FilterData.GroupColor(With: Name)))
-                    .shadow(radius: 5)
             })
         .foregroundColor(.black)
         .padding(.leading, 5)
