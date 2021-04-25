7c11d6598dce3c70d21f2779de1964485352bae5
Stuart Rankin <sjrankin@gmail.com>
2021-04-25 21:09:05 +0900

    Update Instant.swift
    Removed unused code.

--- BlockCam2/Filters/BuiltInFilters/Filters/Instant.swift
+++ BlockCam2/Filters/BuiltInFilters/Filters/Instant.swift
@@ -26,30 +26,6 @@ class Instant: BuiltInFilterProtocol
     static var Name: String = "Instant"
     
     func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
-                   _ ColorSpace: CGColorSpace) -> CVPixelBuffer
-    {
-        let SourceImage = CIImage(cvImageBuffer: Buffer)
-        let Adjust = CIFilter.photoEffectInstant()
-        Adjust.inputImage = SourceImage
-        if let Adjusted = Adjust.outputImage
-        {
-            var PixBuf: CVPixelBuffer? = nil
-            CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool, &PixBuf)
-            guard let OutPixBuf = PixBuf else
-            {
-                fatalError("Allocation failure in \(#function)")
-            }
-            CIContext().render(Adjusted, to: OutPixBuf, bounds: SourceImage.extent,
-                               colorSpace: ColorSpace)
-            return OutPixBuf
-        }
-        else
-        {
-            return Buffer
-        }
-    }
-    
-    func RunFilter(_ Buffer: CVPixelBuffer, _ BufferPool: CVPixelBufferPool,
                    _ ColorSpace: CGColorSpace, Options: [FilterOptions: Any]) -> CVPixelBuffer
     {
         let SourceImage = CIImage(cvImageBuffer: Buffer)
