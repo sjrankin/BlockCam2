00cb7c1991d0474b8a5cb2666216e05a428203ed
Stuart Rankin <sjrankin@gmail.com>
2021-04-25 15:40:40 +0900

    Create ImageSaved.swift
    ImageSaved view.

---
+++ BlockCam2/SubViews/ImageSaved.swift
@@ -0,0 +1,44 @@
+//
+//  ImageSaved.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/24/21.
+//
+
+import SwiftUI
+
+struct ImageSaved: View
+{
+    @Binding var IsVisible: Bool
+    @State var Width: CGFloat
+    @State var Height: CGFloat
+    
+    var body: some View
+    {
+        Text("Image Saved")
+            .foregroundColor(.black)
+            .font(.custom("Avenir-Heavy", size: 30.0))
+            .padding()
+            .background(
+                ZStack
+                {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .stroke(Color.black, lineWidth: 5)
+                        .shadow(radius: 5)
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(Color(UIColor.systemGray2))
+                        .shadow(radius: 3)
+                }
+                .shadow(radius: 5)
+                .frame(width: 375, height: 50)
+            )
+            .frame(width: Width * 0.75,
+                   height: 50.0,
+                   alignment: .top)
+            .position(x: IsVisible ? Width / 2 : -Width / 2,
+                      y: 75 / 2)
+            .animation(.linear(duration: IsVisible ? 0.2 : 0.15))
+            .transition(.slide)
+    }
+}
+
