c4f8b765fe62e9ffd646a64553aaedd5a931041c
Stuart Rankin <sjrankin@gmail.com>
2021-04-28 21:31:13 +0900

    Update LiveViewControllerUI.swift
    Refactoring.

--- BlockCam2/SubViews/LiveViewControllerUI.swift
+++ BlockCam2/SubViews/LiveViewControllerUI.swift
@@ -9,17 +9,26 @@ import Foundation
 import UIKit
 import SwiftUI
 
+enum ChangedDataTypes
+{
+    case None
+    case Button
+    case Setting
+}
+
 /// Wrapper for the live view UI (which requires components not yet available in SwiftUI).
 struct LiveViewControllerUI: UIViewControllerRepresentable
 {
     @Binding var FilterButtonPressed: String
     @Binding var IsSelfieCamera: Bool
+    @Binding var ShowFilterSettings: Bool
+    @Binding var ToggleSavedImageNotice: Bool
     
     /// Returns the LiveViewController instance.
     func makeUIViewController(context: Context) -> LiveViewController
     {
         let ActualController = LiveViewController()
-        ActualController.UIDelegate = context.coordinator
+        ActualController.UIDelegate = context.coordinator //as! ViewControllerDelegate
         return ActualController
     }
     
@@ -28,18 +37,13 @@ struct LiveViewControllerUI: UIViewControllerRepresentable
     {
         uiViewController.ButtonPressed(FilterButtonPressed)
     }
-    
-    func GetFilterData() -> FilterListData
-    {
-        return FilterData
-    }
-    
+   
     func makeCoordinator() -> Coordinator
     {
-        return Coordinator(self)
+        Coordinator(self)
     }
     
-    class Coordinator: NSObject, ViewControllerDelegate
+    class Coordinator: NSObject, ViewControllerDelegate//UIPageViewControllerDelegate
     {
         var Parent: LiveViewControllerUI
         
@@ -48,27 +52,21 @@ struct LiveViewControllerUI: UIViewControllerRepresentable
             self.Parent = Parent
         }
         
-        func FilterNamesPassed(_ viewController: LiveViewController, Names: [String])
+        func ShowFilterSettings(For: String)
         {
-            Parent.FilterData.FilterNames = Names.map({IDString(id: $0, Value: $0)})
+            print("ShowFilterSettings<-\(For)")
+            Parent.ShowFilterSettings = true
+        }
+        
+        func ToggleImageSavedNotice()
+        {
+            Parent.ToggleSavedImageNotice = true
         }
     }
-    
-    @ObservedObject var FilterData = FilterListData()
 }
 
 protocol ViewControllerDelegate: AnyObject
 {
-    func FilterNamesPassed(_ viewController: LiveViewController, Names: [String])
-}
-
-class FilterListData: ObservableObject
-{
-    @Published var FilterNames = [IDString]()
-}
-
-struct IDString: Identifiable
-{
-    var id: String
-    var Value: String
+    func ShowFilterSettings(For: String)
+    func ToggleImageSavedNotice()
 }
