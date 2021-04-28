a88261dc5c42b709ff80e398de61a632a6f49a02
Stuart Rankin <sjrankin@gmail.com>
2021-04-28 21:26:24 +0900

    Update BuiltInFilterProtocol.swift
    Moved FilterOptions to other file. Removed unusual function defintion.

--- BlockCam2/Filters/BuiltInFilters/BuiltInFilterProtocol.swift
+++ BlockCam2/Filters/BuiltInFilters/BuiltInFilterProtocol.swift
@@ -6,26 +6,20 @@
 //
 
 import Foundation
+import CoreMedia
 
 /// Protocol for 2D filters.
 protocol BuiltInFilterProtocol
 {
     /// Returns the type of filter.
     static var FilterType: BuiltInFilters {get}
+
+    /// Tells the caller if the class' `Initialization` function should be called.
+    var NeedsInitialization: Bool {get}
     
-    /// Returns the name of the filter. Deprecated.
-    static var Name: String {get}
-    
-    #if false
-    /// Run the filter.
-    /// - Note: Default values are used for all filter parameters.
-    /// - Parameter Buffer: The source image buffer upon which the filter will be executed.
-    /// - Parameter BufferPool: Managed buffer pool.
-    /// - Parameter Colorspace: Current color space.
-    func RunFilter(_ Buffer: CVPixelBuffer,
-                   _ BufferPool: CVPixelBufferPool,
-                   _ ColorSpace: CGColorSpace) -> CVPixelBuffer
-    #endif
+    /// Description of the intialization function for filters that require it. For filters that do not require
+    /// this function, it is ignored if called.
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
     
     /// Run the filter.
     /// - Parameter Buffer: The source image buffer upon which the filter will be executed.
@@ -40,44 +34,3 @@ protocol BuiltInFilterProtocol
                    _ ColorSpace: CGColorSpace,
                    Options: [FilterOptions: Any]) -> CVPixelBuffer
 }
-
-/// Used to specify optional values for filters. Interpretation of filter options depends on the individual
-/// filter.
-enum FilterOptions: String
-{
-    case Radius = "Radius"
-    case Center = "Center"
-    case Angle = "Angle"
-    case Scale = "Scale"
-    case Zoom = "Zoom"
-    case Rotation = "Roation"
-    case Periodicity = "Periodicity"
-    case Sharpness = "Sharpness"
-    case Width = "Width"
-    case Height = "Height"
-    case ExposureValue = "ExposureValue"
-    case Color = "Color"
-    case Color0 = "Color0"
-    case Color1 = "Color1"
-    case GrayscaleFilter = "GrayscaleFilter"
-    case Count = "Count"
-    case Contrast = "Contrast"
-    case Threshold = "Threshold"
-    case EdgeIntensity = "EdgeIntensity"
-    case NRNoiseLevel = "NRNoiseLevel"
-    case NRSharpness = "NRSharpness"
-    case Levels = "Levels"
-    case Point = "Point"
-    case Amount = "Amount"
-    case Point1 = "Point1"
-    case Point2 = "Point2"
-    case Strands = "Strands"
-    case Intensity = "Intensity"
-    case Size = "Size"
-    case GradientColor0 = "GradientColor0"
-    case GradientColor1 = "GradientColor1"
-    case GradientPoint0 = "GradientPoint0"
-    case GradientPoint1 = "GradientPoint1"
-    case ChainedFilters = "ChainedFilters"
-}
-
