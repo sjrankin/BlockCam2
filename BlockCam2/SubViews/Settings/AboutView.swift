57047f1516856fda6faf902c90d54570df6830a0
Stuart Rankin <sjrankin@gmail.com>
2021-04-28 21:31:59 +0900

    Create AboutView.swift
    Placeholder for about.

---
+++ BlockCam2/SubViews/Settings/AboutView.swift
@@ -0,0 +1,16 @@
+//
+//  AboutView.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/27/21.
+//
+
+import SwiftUI
+
+struct AboutView: View
+{
+    var body: some View
+    {
+        Text("About page")
+    }
+}
