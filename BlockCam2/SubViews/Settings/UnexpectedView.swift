3d09394a62409e3fa116447d7af328541a429dfe
Stuart Rankin <sjrankin@gmail.com>
2021-04-28 21:32:58 +0900

    Create UnexpectedView.swift
    Placeholder for filter settings when unknown filter selected.

---
+++ BlockCam2/SubViews/Settings/UnexpectedView.swift
@@ -0,0 +1,16 @@
+//
+//  UnexpectedView.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/27/21.
+//
+
+import SwiftUI
+
+struct UnexpectedView: View
+{
+    var body: some View
+    {
+        Text("Huh?")
+    }
+}
