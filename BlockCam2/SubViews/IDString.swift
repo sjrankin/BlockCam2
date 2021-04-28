5626f81d6457593644911d8d2afd6170a8db8831
Stuart Rankin <sjrankin@gmail.com>
2021-04-28 21:31:24 +0900

    Create IDString.swift
    Split out into its own file.

---
+++ BlockCam2/SubViews/IDString.swift
@@ -0,0 +1,14 @@
+//
+//  IDString.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/27/21.
+//
+
+import SwiftUI
+
+struct IDString: Identifiable
+{
+    var id: String
+    var Value: String
+}
