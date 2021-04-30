a9350130bfa5c0ecf50d49d8c23fc339c703394c
Stuart Rankin <sjrankin@gmail.com>
2021-04-30 20:00:51 +0900

    Create NoFilter_View.swift
    View for no settings for filter.

---
+++ BlockCam2/SubViews/Settings/FilterSettings2/FilterSettingViews/NoFilter_View.swift
@@ -0,0 +1,23 @@
+//
+//  NoFilter_View.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 4/30/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct NoFilter_View: View
+{
+    @State var FilterName: String
+    
+    var body: some View
+    {
+        ScrollView
+        {
+            Text("No settings for \(FilterName)")
+                .padding()
+        }
+    }
+}
