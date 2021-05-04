5ccfc5d514c586f97c3ca4a6d62669f592e2fee7
Stuart Rankin <sjrankin@gmail.com>
2021-05-04 20:53:10 +0900

    Create SettingDefaults.swift
    Added array to Settings that will contain the default values of all settings.

---
+++ BlockCam2/Settings/SettingDefaults.swift
@@ -0,0 +1,49 @@
+//
+//  SettingDefaults.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/4/21.
+//
+
+import Foundation
+import UIKit
+
+extension Settings
+{
+    public static let SettingDefaults: [SettingKeys: Any] =
+        [
+            // Infrastructure/initialization-related settings.
+            .InitializationFlag: false,
+            .InstanceID: "",
+            
+            // User interface settings.
+            .ShowAudioWaveform: false,
+            .SaveOriginalImage: true,
+            
+            // Filter settings.
+            .CurrentFilter: "",
+            .CurrentGroup: "",
+            
+            // Hue Adjust.
+            .HueAngle: 135.0,
+            
+            // Kaleidoscope.
+            .KaleidoscopeSegmentCount: 30,
+            .KaleidoscopeAngleOfReflection: 90.0,
+            
+            // Triangular Kaleidoscope.
+            .Kaleidoscope3Rotation: 0.0,
+            .Kaleidoscope3Size: 200.0,
+            .Kaleidoscope3Decay: 1.0,
+            
+            // Mirroring.
+            .MirrorDirection: 0,
+            .MirrorLeft: true,
+            .MirrorTop: true,
+            .MirrorQuadrant: 1,
+            .QuadrantsRotated: true,
+            
+            // Color Map.
+            .ColorMapGradient: "(White)@(0.0),(Black)@(1.0)",
+        ]
+}
