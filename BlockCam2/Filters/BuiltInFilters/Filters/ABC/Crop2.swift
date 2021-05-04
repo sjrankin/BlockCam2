100d1af66181b230108d6b4cf46342739a596d64
Stuart Rankin <sjrankin@gmail.com>
2021-05-04 20:56:27 +0900

    Create Crop2.swift
    Test filter for cropping images.

---
+++ BlockCam2/Filters/BuiltInFilters/Filters/ABC/Crop2.swift
@@ -0,0 +1,84 @@
+//
+//  Crop2.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/2/21.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import AVFoundation
+import CoreImage
+import CoreServices
+import AVKit
+import Photos
+import MobileCoreServices
+import CoreMotion
+import CoreMedia
+import CoreVideo
+import CoreImage.CIFilterBuiltins
+
+class Crop2: BuiltInFilterProtocol
+{
+    static var FilterType: BuiltInFilters = .Crop2
+    
+    var NeedsInitialization: Bool = false
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+    }
+    
+    static func RunFilter(_ Buffer: CVPixelBuffer, Options: [FilterOptions: Any]) -> CIImage?
+    {
+        let SourceImage = CIImage(cvPixelBuffer: Buffer)
+        if let Adjust = CIFilter(name: "CICrop")
+        {
+            Adjust.setValue(SourceImage, forKey: kCIInputImageKey)
+            let Vector = CGRect(origin: CGPoint.zero, size: CGSize(width: SourceImage.extent.width / 2.0,
+                                                                   height: SourceImage.extent.height / 2.0))
+            Adjust.setValue(Vector, forKey: "inputRectangle")
+            if let Adjusted = Adjust.outputImage
+            {
+                return Adjusted
+            }
+        }
+        return nil
+    }
+    
+    func RunFilter(_ Buffer: [CVPixelBuffer], _ BufferPool: CVPixelBufferPool,
+                   _ ColorSpace: CGColorSpace, Options: [FilterOptions: Any]) -> CVPixelBuffer
+    {
+        let SourceImage = CIImage(cvImageBuffer: Buffer.first!)
+        if let Adjust = CIFilter(name: "CICrop")
+        {
+            Adjust.setValue(SourceImage, forKey: kCIInputImageKey)
+            let Vector = CGRect(origin: CGPoint.zero, size: CGSize(width: SourceImage.extent.width / 2.0,
+                                                                   height: SourceImage.extent.height / 2.0))
+            Adjust.setValue(Vector, forKey: "inputRectangle")
+            if let Adjusted = Adjust.outputImage
+            {
+                var PixBuf: CVPixelBuffer? = nil
+                CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool, &PixBuf)
+                guard let OutPixBuf = PixBuf else
+                {
+                    fatalError("Allocation failure in \(#function)")
+                }
+                CIContext().render(Adjusted, to: OutPixBuf, bounds: Vector,
+                                   colorSpace: ColorSpace)
+                let test = CIImage(cvPixelBuffer: OutPixBuf)
+                return OutPixBuf
+            }
+            else
+            {
+                print("No output image available")
+                return Buffer.first!
+            }
+        }
+        else
+        {
+            print("Did not find CICrop")
+            return Buffer.first!
+        }
+    }
+}
