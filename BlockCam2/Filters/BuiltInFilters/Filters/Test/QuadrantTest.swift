d26e69f8d92a3c26a73955057cabe1028e30a970
Stuart Rankin <sjrankin@gmail.com>
2021-05-04 20:57:05 +0900

    Create QuadrantTest.swift
    Quadrant test filter.

---
+++ BlockCam2/Filters/BuiltInFilters/Filters/Test/QuadrantTest.swift
@@ -0,0 +1,52 @@
+//
+//  QuadrantTest.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/2/21.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import AVFoundation
+import CoreImage
+import CoreServices
+import AVKit
+import Photos
+import MobileCoreServices
+import CoreMotion
+import CoreMedia
+import CoreVideo
+import CoreImage.CIFilterBuiltins
+
+class QuadrantTest: BuiltInFilterProtocol
+{
+    static var FilterType: BuiltInFilters = .QuadrantTest
+    
+    var NeedsInitialization: Bool = false
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+    }
+    
+    func RunFilter(_ Buffer: [CVPixelBuffer], _ BufferPool: CVPixelBufferPool,
+                   _ ColorSpace: CGColorSpace, Options: [FilterOptions: Any]) -> CVPixelBuffer
+    {
+        let C = Crop2().RunFilter(Buffer, BufferPool, ColorSpace, Options: Options)
+        let test = CIImage(cvPixelBuffer: C)
+        return C
+        if let CroppedImage = Crop2.RunFilter(Buffer.first!, Options: Options)
+        {
+            if let PixBuf = MetalFilterParent.GetPixelBufferFrom(CroppedImage)
+            {
+                let Final = Filters.RunFilter(.Reflect, With: PixBuf, Options: Options)
+                return Final!
+            }
+            else
+            {
+                fatalError("GetPixelBufferFrom returned nil")
+            }
+        }
+        return Buffer.first!
+    }
+}
