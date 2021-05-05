c978a7749cc1ba6afb61f3f33b61b5830e835645
Stuart Rankin <sjrankin@gmail.com>
2021-05-05 20:45:59 +0900

    Update TriangleKaleidoscopeFilter_View.swift
    Working on updating UI for new options handling.

--- BlockCam2/SubViews/Settings/FilterSettings2/FilterSettingViews/TriangleKaleidoscopeFilter_View.swift
+++ BlockCam2/SubViews/Settings/FilterSettings2/FilterSettingViews/TriangleKaleidoscopeFilter_View.swift
@@ -10,10 +10,19 @@ import SwiftUI
 
 struct TriangleKaleidoscopeFilter_View: View
 {
+    @State var Updated: Bool = false
+    @State var CurrentSize: String = "\(Settings.GetInt(.Kaleidoscope3Size))"
+    @State var ActualSize: Int = Settings.GetInt(.Kaleidoscope3Size)
     @State var CurrentAngle: String = "\(Settings.GetInt(.Kaleidoscope3Rotation))"
     @State var ActualAngle: Double = Double(Settings.GetInt(.Kaleidoscope3Rotation))
     @State var CurrentFade: String = "\(Settings.GetDouble(.Kaleidoscope3Decay, 0.0))"
     @State var ActualFade: Double = Double(Settings.GetDouble(.Kaleidoscope3Decay, 0.0))
+    @State var Options: [FilterOptions: Any] =
+        [
+            .Decay: Settings.GetDouble(.Kaleidoscope3Decay, 0.0),
+         .Rotation: Double(Settings.GetInt(.Kaleidoscope3Rotation)),
+            .Size: Settings.GetInt(.Kaleidoscope3Size)
+        ]
     
     var body: some View
     {
@@ -28,13 +37,45 @@ struct TriangleKaleidoscopeFilter_View: View
                     {
                         VStack(alignment: .leading)
                         {
+                            Text("Size")
+                            Text("Size of the triangle")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        .padding()
+                        
+                        Spacer()
+                        
+                        VStack
+                        {
+                            TextField("0", text: $CurrentSize,
+                                      onCommit:
+                                        {
+                                            if let Actual = Int(self.CurrentSize)
+                                            {
+                                                ActualSize = Actual
+                                                Settings.SetInt(.Kaleidoscope3Size, ActualSize)
+                                                Options[.Size] = ActualSize
+                                            }
+                                        })
+                                .padding()
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .font(.custom("Avenir-Black", size: 18.0))
+                                .keyboardType(.numbersAndPunctuation)
+                        }
+                    }
+                    .padding()
+                    
+                    HStack
+                    {
+                        VStack(alignment: .leading)
+                        {
                             Text("Rotation angle")
                             Text("Enter the rotation angle in degrees")
                                 .font(.subheadline)
                                 .foregroundColor(.gray)
                         }
                         .padding()
-                        .frame(width: Geometry.size.width * 0.6)
                         
                         Spacer()
                         
@@ -47,12 +88,12 @@ struct TriangleKaleidoscopeFilter_View: View
                                             {
                                                 ActualAngle = Double(Int(Actual))
                                                 Settings.SetInt(.Kaleidoscope3Rotation, Int(Actual))
+                                                Options[.Angle] = Actual
                                             }
                                         })
                                 .padding()
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .font(.custom("Avenir-Black", size: 18.0))
-                                .frame(width: Geometry.size.width * 0.35)
                                 .keyboardType(.numbersAndPunctuation)
                             
                             Slider(value: $ActualAngle, in: 0 ... 359,
@@ -63,11 +104,13 @@ struct TriangleKaleidoscopeFilter_View: View
                                         {
                                             CurrentAngle = "\(Int(ActualAngle))"
                                             Settings.SetInt(.Kaleidoscope3Rotation, Int(ActualAngle))
+                                            Options[.Angle] = ActualAngle
                                         }
                                     })
                                 .frame(width: Geometry.size.width * 0.30)
                         }
                     }
+                    .padding()
                     
                     HStack
                     {
@@ -79,7 +122,6 @@ struct TriangleKaleidoscopeFilter_View: View
                                 .foregroundColor(.gray)
                         }
                         .padding()
-                        .frame(width: Geometry.size.width * 0.6)
                         
                         Spacer()
                         
@@ -92,12 +134,12 @@ struct TriangleKaleidoscopeFilter_View: View
                                             {
                                                 ActualFade = Double(Actual)
                                                 Settings.SetDouble(.Kaleidoscope3Decay, Actual)
+                                                Options[.Decay] = Actual
                                             }
                                         })
                                 .padding()
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .font(.custom("Avenir-Black", size: 18.0))
-                                .frame(width: Geometry.size.width * 0.35)
                                 .keyboardType(.numbersAndPunctuation)
                             
                             Slider(value: $ActualFade, in: 0 ... 359,
@@ -107,14 +149,35 @@ struct TriangleKaleidoscopeFilter_View: View
                                         if !Editing
                                         {
                                             CurrentFade = "\(ActualFade)"
-                                            Settings.SetDouble(.Kaleidoscope3Decay, ActualAngle)
+                                            Settings.SetDouble(.Kaleidoscope3Decay, ActualFade)
+                                            Options[.Decay] = ActualFade
                                         }
                                     })
                                 .frame(width: Geometry.size.width * 0.30)
                         }
                     }
+                    .padding()
+                    
+                    Spacer()
+                    Spacer()
+                    #if true
+                    SampleImage(Filter: .TriangleKaleidoscope, Updated: $Updated.wrappedValue)
+                        .frame(width: 300, height: 300, alignment: .center)
+                    #else
+                    SampleImage(FilterOptions: $Options, Filter: .TriangleKaleidoscope)
+                        .frame(width: 300, height: 300, alignment: .center)
+                    #endif
                 }
             }
         }
+    }
+}
+
+
+struct TriangleKaleidoscopeFilter_Preview: PreviewProvider
+{
+    static var previews: some View
+    {
+        TriangleKaleidoscopeFilter_View()
     }
 }
