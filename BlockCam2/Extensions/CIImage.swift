644fa837928828d9d4dd5e6202bcb5a2cb76a220
Stuart Rankin <sjrankin@gmail.com>
2021-05-12 16:35:38 +0900

    Update CIImage.swift
    Added rotation and mirroring functions to handle images from the photo library that are oriented oddly.

--- BlockCam2/Extensions/CIImage.swift
+++ BlockCam2/Extensions/CIImage.swift
@@ -24,4 +24,82 @@ extension CIImage
         }
         return nil
     }
+    
+    /// Rotate the instance image by 90° left.
+    /// - Parameter AndMirror: If true, the image is mirrored with `.upMirrored` before
+    ///                        rotating it leftwards.
+    /// - Returns: Rotated and potentially mirrored image.
+    func RotateLeft(AndMirror: Bool = false) -> CIImage
+    {
+        if AndMirror
+        {
+            var Rotated = self.oriented(.upMirrored)
+            Rotated = Rotated.oriented(.left)
+            return Rotated
+        }
+        return self.oriented(.left)
+    }
+    
+    /// Mirror the image left.
+    /// - Returns: Image mirrored left.
+    func MirrorLeft() -> CIImage
+    {
+        let Mirrored = self.oriented(.leftMirrored)
+        return Mirrored
+    }
+    
+    /// Rotate the instance image by 90° right.
+    /// - Parameter AndMirror: If true, the image is mirrored with `.downMirrored` before
+    ///                        rotating it rightwards.
+    /// - Returns: Rotated and potentially mirrored image.
+    func RotateRight(AndMirror: Bool = false) -> CIImage
+    {
+        if AndMirror
+        {
+            var Rotated = self.oriented(.downMirrored)
+            Rotated = Rotated.oriented(.right)
+            return Rotated
+        }
+        return self.oriented(.right)
+    }
+    
+    /// Mirror the image right.
+    /// - Returns: Image mirrored right.
+    func MirrorRight() -> CIImage
+    {
+        let Mirrored = self.oriented(.rightMirrored)
+        return Mirrored
+    }
+    
+    /// Mirror the image up.
+    /// - Returns: Image mirrored up.
+    func MirrorUp() -> CIImage
+    {
+        let Mirrored = self.oriented(.upMirrored)
+        return Mirrored
+    }
+    
+    /// Mirror the image down.
+    /// - Returns: Image mirrored down.
+    func MirrorDown() -> CIImage
+    {
+        let Mirrored = self.oriented(.downMirrored)
+        return Mirrored
+    }
+    
+    /// Rotate the instance image by 180°.
+    /// - Parameter AndMirror: If true, the image is mirrored with `.leftMirrored` before
+    ///                        being rotated 180°.
+    /// - Returns: Rotated image.
+    func Rotate180(AndMirror: Bool = false) -> CIImage
+    {
+        if AndMirror
+        {
+            //var Rotated = self.oriented(.leftMirrored)
+            let Rotated = self.oriented(.downMirrored)
+            return Rotated
+        }
+        let Rotated = self.oriented(.down)
+        return Rotated
+    }
 }
