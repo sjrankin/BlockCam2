b04bb48384b795dd4dc33cb97d5e9b66808f7b69
Stuart Rankin <sjrankin@gmail.com>
2021-05-17 20:53:45 +0900

    Update SettingsUIData.swift
    Added new settings definitions for the UI.

--- BlockCam2/SubViews/Settings/SettingsUIData.swift
+++ BlockCam2/SubViews/Settings/SettingsUIData.swift
@@ -17,10 +17,29 @@ struct OptionItem: Identifiable
 
 let SettingsData =
     [
+        OptionItem(Title: "About",
+                   Annotation: "About BlockCam",
+                   Picture: "info.circle"),
         OptionItem(Title: "Settings",
                    Annotation: "General program settings",
                    Picture: "gearshape"),
-        OptionItem(Title: "About",
-                   Annotation: "About BlockCam",
-                   Picture: "info.circle")
+        OptionItem(Title: "Camera",
+                   Annotation: "Camera and image settings",
+                   Picture: "camera"),
+        OptionItem(Title: "Sample Images",
+                   Annotation: "Manage sample images.",
+                   Picture: "photo.on.rectangle")
     ]
+
+let SampleImageOptions =
+[
+    OptionItem(Title: "Built-in",
+               Annotation: "Manage built-in sample images",
+               Picture: "photo"),
+    OptionItem(Title: "Your images",
+               Annotation: "Manage you own sample images",
+               Picture: "person.crop.circle"),
+    OptionItem(Title: "Other options",
+               Annotation: "Other options for sample images",
+               Picture: "circle.dashed.inset.fill")
+]
