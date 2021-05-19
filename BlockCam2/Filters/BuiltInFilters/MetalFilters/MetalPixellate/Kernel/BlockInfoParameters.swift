2f18c2435f64144fcf05c1a7d77a6230a587c197
Stuart Rankin <sjrankin@gmail.com>
2021-05-19 23:05:48 +0900

    Create BlockInfoParameters.swift
    Parameter block for Metal pixellate.

---
+++ BlockCam2/Filters/BuiltInFilters/MetalFilters/MetalPixellate/Kernel/BlockInfoParameters.swift
@@ -0,0 +1,22 @@
+//
+//  BlockInfoParameters.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/19/21.
+//
+
+import Foundation
+import simd
+
+struct BlockInfoParameters
+{
+    let Width: simd_uint1
+    let Height: simd_uint1
+    let HighlightAction: simd_uint1
+    let HighlightPixelBy: simd_uint1
+    let BrightnessHighlight: simd_uint1
+    let HighlightColor: simd_float4
+    let ColorDetermination: simd_uint1
+    let HighlightValue: simd_float1
+    let HighlightIfGreater: simd_bool
+}
