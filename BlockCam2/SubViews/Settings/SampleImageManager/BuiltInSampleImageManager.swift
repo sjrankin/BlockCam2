b18688972d455eca88f7b04f3dc370b7fffc9104
Stuart Rankin <sjrankin@gmail.com>
2021-05-19 22:48:35 +0900

    Update BuiltInSampleImageManager.swift
    Refactored.

--- BlockCam2/SubViews/Settings/SampleImageManager/BuiltInSampleImageManager.swift
+++ BlockCam2/SubViews/Settings/SampleImageManager/BuiltInSampleImageManager.swift
@@ -10,14 +10,6 @@ import SwiftUI
 struct BuiltInSampleImageManager: View
 {
     @Environment(\.presentationMode) var presentionMode: Binding<PresentationMode>
-    @State  var Sample1Enabled: Bool = true
-    @State  var Sample2Enabled: Bool = true
-    @State  var Sample3Enabled: Bool = true
-    @State  var Sample4Enabled: Bool = true
-    @State  var Sample5Enabled: Bool = true
-    @State  var Sample6Enabled: Bool = true
-    @State  var Sample7Enabled: Bool = true
-    @State  var Sample8Enabled: Bool = true
     
     var body: some View
     {
@@ -26,67 +18,20 @@ struct BuiltInSampleImageManager: View
             Reader in
             ScrollView
             {
-            LazyVStack
-            {
-                Group
+                LazyVStack
                 {
-                    BultInImageTableEntryView(ImageName: "Sample1",
-                                              ImageDescription: "Sample 1",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample1Enabled,
-                                              CanDisable: false)
-                    Divider()
-                        .background(Color.black)
-                    BultInImageTableEntryView(ImageName: "Sample2",
-                                              ImageDescription: "Sample 2",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample2Enabled)
-                    Divider()
-                        .background(Color.black)
-                    BultInImageTableEntryView(ImageName: "Sample3",
-                                              ImageDescription: "Sample 3",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample3Enabled)
-                    Divider()
-                        .background(Color.black)
-                    BultInImageTableEntryView(ImageName: "Sample4",
-                                              ImageDescription: "Sample 4",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample4Enabled)
-                    Divider()
-                        .background(Color.black)
+                    ForEach(0 ..< SampleImages.BuiltInSamples.count, id: \.self)
+                    {
+                        Index in
+                        BultInImageTableEntryView2(ImageName: SampleImages.BuiltInSamples[Index].SampleName,
+                                                   ImageDescription: SampleImages.BuiltInSamples[Index].Title,
+                                                   Attribution: SampleImages.BuiltInSamples[Index].Attribution,
+                                                   OverallWidth: Reader.size.width)
+                    }
                 }
-                Group
-                {
-                    BultInImageTableEntryView(ImageName: "Sample5",
-                                              ImageDescription: "Sample 5",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample5Enabled)
-                    Divider()
-                        .background(Color.black)
-                    BultInImageTableEntryView(ImageName: "Sample6",
-                                              ImageDescription: "Sample 6",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample6Enabled)
-                    Divider()
-                        .background(Color.black)
-                    BultInImageTableEntryView(ImageName: "Sample7",
-                                              ImageDescription: "Sample 7",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample7Enabled)
-                    Divider()
-                        .background(Color.black)
-                    BultInImageTableEntryView(ImageName: "Sample8",
-                                              ImageDescription: "Sample 8",
-                                              OverallWidth: Reader.size.width,
-                                              ImageIsEnabled: Sample8Enabled)
-                    Divider()
-                        .background(Color.black)
-                }
-            }
             }
             .navigationBarTitleDisplayMode(.inline)
-            .navigationBarTitle(Text("Bulit-in Sample Images"))
+            .navigationBarTitle(Text("Built-in Sample Images"))
             .toolbar
             {
                 ToolbarItem(placement: .navigationBarTrailing)
