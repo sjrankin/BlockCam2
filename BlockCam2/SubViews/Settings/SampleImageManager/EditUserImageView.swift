494919a474e9890111597434f85a71ecbd0714bd
Stuart Rankin <sjrankin@gmail.com>
2021-05-22 11:54:40 +0900

    Create EditUserImageView.swift
    Added view for users to edit descriptions for their sample images.

---
+++ BlockCam2/SubViews/Settings/SampleImageManager/EditUserImageView.swift
@@ -0,0 +1,126 @@
+//
+//  EditUserImageView.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/22/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct EditUserImageView: View
+{
+    @State var FileExists: Bool = false
+    @State var Updated: Bool = false
+    @State var Description: String = ""
+    @State var SelectedImageName: String = ""
+    @State var SelectedImage: UIImage = UIImage()
+    @State var SelectedImageURL: URL? = nil
+    @Environment(\.presentationMode) var presentionMode: Binding<PresentationMode>
+    
+    init(SelectedImageName: String, SelectedImage: UIImage, Description: String,
+         _ Block: ((Bool) -> ())? = nil)
+    {
+        Closure = Block
+        self.SelectedImageName = SelectedImageName
+        self.SelectedImage = SelectedImage
+        self.Description = Description
+    }
+    
+    var Closure: ((Bool) -> ())? = nil
+    
+    var body: some View
+    {
+        GeometryReader
+        {
+            Geometry in
+            VStack(alignment: .leading)
+            {
+                Text("Description")
+                    .font(.headline)
+                    .frame(width: Geometry.size.width * 0.9,
+                           alignment: .leading)
+                    .padding([.top, .leading, .trailing])
+                TextField("", text: Binding(
+                    get:
+                        {
+                            self.Description
+                        },
+                    set:
+                        {
+                            NewValue in
+                            self.Description = NewValue
+                        }
+                ))
+                .font(.system(size: 18, weight: .regular, design: .default))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: Geometry.size.width * 0.9,
+                       alignment: .leading)
+                .padding([.leading, .trailing])
+                
+                Divider()
+                    .background(Color.black)
+                
+                Spacer()
+                
+                VStack(alignment: .center)
+                {
+                    Image(uiImage: SelectedImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .border(Color.black, width: 0.5)
+                        .frame(alignment: .center)
+                }
+                .padding()
+                
+                Divider()
+                    .background(Color.black)
+                
+                HStack
+                {
+                    Button(action:
+                            {
+                                SampleImages.EditUserSample(FileName: SelectedImageName,
+                                                            Description: Description)
+                                print("OK clicked")
+                                Closure?(true)
+                                self.presentionMode.wrappedValue.dismiss()
+                            })
+                    {
+                        Text("OK")
+                            .font(.custom("Avenir-Heavy", size: 20.0))
+                    }
+                    .alert(isPresented: $FileExists)
+                    {
+                        Alert(title: Text("Duplicate Image"),
+                              message: Text("The image you selected has the same name as an image already in your list. Please select a different image."),
+                              dismissButton: .default(Text("OK"))
+                        )
+                    }
+                    Spacer()
+                    Button(action:
+                            {
+                                print("Cancel pressed")
+                                Closure?(false)
+                                self.presentionMode.wrappedValue.dismiss()
+                            })
+                    {
+                        Text("Cancel")
+                            .font(.custom("Avenir-Heavy", size: 20.0))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct EditUserImageView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        EditUserImageView(SelectedImageName: SampleImages.BuiltInSamples[0].SampleName,
+                          SelectedImage: UIImage(named: SampleImages.CurrentSample.SampleName)!,
+                          Description: "Description of your sample image")
+    }
+}
