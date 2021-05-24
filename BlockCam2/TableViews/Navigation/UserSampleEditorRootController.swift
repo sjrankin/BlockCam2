8c919d2ada2110c864c2639431718585a839661d
Stuart Rankin <sjrankin@gmail.com>
2021-05-24 10:46:23 +0900

    Create UserSampleEditorRootController.swift
    Test code.

---
+++ BlockCam2/TableViews/Navigation/UserSampleEditorRootController.swift
@@ -0,0 +1,17 @@
+//
+//  UserSampleEditorRootController.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/23/21.
+//
+
+import Foundation
+import UIKit
+
+class UserSampleEditorRootController: UINavigationController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
