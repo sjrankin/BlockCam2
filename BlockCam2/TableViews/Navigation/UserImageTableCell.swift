f617b3762eb184f61cf32c6cfef44159cb8fd3c8
Stuart Rankin <sjrankin@gmail.com>
2021-05-24 10:46:43 +0900

    Create UserImageTableCell.swift
    Test code.

---
+++ BlockCam2/TableViews/Navigation/UserImageTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserImageTableCell.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/23/21.
+//
+
+import Foundation
+import UIKit
+
+class UserImageTableCell: UITableViewCell
+{
+    @IBOutlet weak var UserImageThumbnail: UIImageView!
+    @IBOutlet weak var UserImageTitle: UILabel!
+}
