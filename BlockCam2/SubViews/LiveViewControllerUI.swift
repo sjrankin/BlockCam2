b8e7849822e27c95987678121716fcca9a237171
Stuart Rankin <sjrankin@gmail.com>
2021-05-26 18:16:26 +0900

    Update LiveViewControllerUI.swift
    Added comments. ShowShortMessage now displays the short messages on the main queue.

--- BlockCam2/SubViews/LiveViewControllerUI.swift
+++ BlockCam2/SubViews/LiveViewControllerUI.swift
@@ -11,6 +11,11 @@ import SwiftUI
 
 
 /// Wrapper for the live view UI (which requires components not yet available in SwiftUI).
+/// - Parameter UICommand: Command to execute in the live view code.
+/// - Parameter IsSelfieCamera: Determines which camera to use (front or back).
+/// - Parameter ShowFilterSettings: Show or hide filter settings.
+/// - Parameter ShowShorMessageView: Display the short message view.
+/// - Parameter ShortMessage: Message to display in the short message view.
 struct LiveViewControllerUI: UIViewControllerRepresentable
 {
     @Binding var UICommand: String
@@ -61,10 +66,14 @@ struct LiveViewControllerUI: UIViewControllerRepresentable
         }
         
         /// Called when the live view wants to show the "image saved" message.
+        // https://www.reddit.com/r/SwiftUI/comments/e5100l/what_does_the_runtime_warning_modifying_state/
         func ShowShortMessage(With Text: String)
         {
-            Parent.ShortMessage = Text
-            Parent.ShowShortMessageView = true
+            DispatchQueue.main.async
+            {
+                self.Parent.ShortMessage = Text
+                self.Parent.ShowShortMessageView = true
+            }
         }
         
         /// Unconditionally hide the short status message.
@@ -102,16 +111,27 @@ protocol ViewControllerDelegate: AnyObject
     func HideShortMessage(With Delay: Double)
 }
 
+/// Commands from the UI (SwiftUI) to the live view (UIKit), where they are executed.
 enum UICommands: String
 {
+    /// Camera button pressed.
     case TakePicture = "TakePicture"
+    /// Save a still image.
     case SaveStill = "SaveStill"
+    /// Open the photo album and select an image there.
     case SelectFromAlbum = "SelectFromAlbum"
+    /// Switch cameras (front or back).
     case ToggleCamera = "ToggleCamera"
+    /// Set new filter selected by the user.
     case SelectFilter = "Filters"
+    /// Share the image somewhere.
     case ShareImage = "ShareImage"
+    /// Save the sample image, unchanged.
     case SaveOriginalSample = "SaveOriginalSample"
+    /// Save the sample image, filtered.
     case SaveFilteredSample = "SaveFilteredSample"
+    /// Set the live view mode.
     case SetLiveViewMode = "SetLiveViewMode"
+    /// Set the still image mode.
     case SetStillImageMode = "SetStillImageMode"
 }
