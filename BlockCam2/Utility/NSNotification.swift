26668e09e164c33e7f11d88489f67bea1d46f296
Stuart Rankin <sjrankin@gmail.com>
2021-05-26 18:10:55 +0900

    Create NSNotification.swift
    Defines event names we use for notifications.

---
+++ BlockCam2/Utility/NSNotification.swift
@@ -0,0 +1,15 @@
+//
+//  NSNotification.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/25/21.
+//
+
+import Foundation
+
+extension NSNotification
+{
+    static let TitleUpdate = NSNotification.Name.init("TitleUpdate")
+    static let FilterUpdate = NSNotification.Name.init("FilterUpdate")
+    static let GroupUpdate = NSNotification.Name.init("GroupUpdate")
+}
