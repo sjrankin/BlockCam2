9e5c8f3643712912ef0a793b5d0d66ad677d5417
Stuart Rankin <sjrankin@gmail.com>
2021-05-26 18:21:44 +0900

    Update LineScreen.swift
    Added option handling.

--- BlockCam2/Filters/BuiltInFilters/Filters/JKLM/LineScreen.swift
+++ BlockCam2/Filters/BuiltInFilters/Filters/JKLM/LineScreen.swift
@@ -35,7 +35,9 @@ class LineScreen: CIFilterBase, BuiltInFilterProtocol
         let SourceImage = CIImage(cvImageBuffer: Buffer.first!)
         super.CreateBufferPool(Source: SourceImage, From: Buffer.first!)
         let Adjust = CIFilter.lineScreen()
-        Adjust.angle = Float(Options[.Angle] as? Double ?? 0.0)
+        var FinalAngle = Options[.Angle] as? Double ?? 0.0
+        FinalAngle = FinalAngle * Double.pi / 180.0
+        Adjust.angle = Float(FinalAngle)
         Adjust.center = Options[.Center] as? CGPoint ?? CGPoint(x: SourceImage.extent.width / 2.0,
                                                                 y: SourceImage.extent.height / 2.0)
         Adjust.inputImage = SourceImage
@@ -60,5 +62,31 @@ class LineScreen: CIFilterBase, BuiltInFilterProtocol
     /// Reset the filter's settings.
     static func ResetFilter()
     {
+        Settings.SetDouble(.LineScreenAngle, Settings.SettingDefaults[.LineScreenAngle] as! Double)
+    }
+    
+    /// Returns the initial preset angle for the setting segment control. If the angle does not equal
+    /// a preset value, 0 is returned.
+    /// - Returns: Angle to use for the preset segment control for the line screen angle.
+    public static func GetInitialPresetAngle() -> Int
+    {
+        let Current = Settings.GetDouble(.LineScreenAngle, 0.0)
+        if Current == 0.0
+        {
+            return 0
+        }
+        if Current == 45.0
+        {
+            return 45
+        }
+        if Current == 90.0
+        {
+            return 90
+        }
+        if Current == 135.0
+        {
+            return 135
+        }
+        return 0
     }
 }
