0fd8ef0e24dbaf71540a55cd8f1c49af2a24bc92
Stuart Rankin <sjrankin@gmail.com>
2021-05-27 15:34:14 +0900

    Create CircularProgressView.swift
    Circular progress indicator to take the place of the buggy built-in version.

---
+++ BlockCam2/SubViews/CircularProgressView.swift
@@ -0,0 +1,44 @@
+//
+//  CircularProgressView.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/27/21.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View
+{
+    @Binding var Percent: Double
+    @State var ForegroundColor: Color = .blue
+    @State var BackgroundColor: Color = Color(.systemGray4)
+    @State var Width: CGFloat = 24.0
+    @State var Height: CGFloat = 24.0
+    @State var StrokeThickness: CGFloat = 5.0
+    
+    var body: some View
+    {
+        ZStack
+        {
+            Circle()
+                .stroke(BackgroundColor, lineWidth: StrokeThickness)
+            Circle()
+                .trim(from: 0, to: CGFloat(self.Percent))
+                .stroke(ForegroundColor,
+                        style: StrokeStyle(lineWidth: StrokeThickness,
+                                           lineCap: .round))
+        }
+        .rotationEffect(Angle(degrees: -90))
+        .frame(width: Width, height: Height)
+        .padding()
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider
+{
+    @State static var Value: Double = 0.61
+    static var previews: some View
+    {
+        CircularProgressView(Percent: $Value)
+    }
+}
