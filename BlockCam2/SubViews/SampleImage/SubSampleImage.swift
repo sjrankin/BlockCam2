0d776ed15397765e29c83eacda0a8b2861ea068d
Stuart Rankin <sjrankin@gmail.com>
2021-05-29 14:58:01 +0900

    Create SubSampleImage.swift
    View for sub-sample (same sized images) image view.

---
+++ BlockCam2/SubViews/SampleImage/SubSampleImage.swift
@@ -0,0 +1,75 @@
+//
+//  SubSampleImage.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/5/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct SubSampleImage: View
+{
+    @State var Key: SettingKeys
+    @Binding var UICommand: String
+    @Binding var ImageName: String
+    @State var Updated: Bool
+    @Binding var ImageTitle: String
+    
+    var body: some View
+    {
+        VStack
+        {
+            //Text(SampleImages.GetCurrentSubSampleImageName(From: $ImageName.wrappedValue))
+            Text(ImageTitle)
+                .frame(alignment: .center)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            Image(uiImage: UIImage(named: SampleImages.GetSubSampleImageName(At: Key))!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .background(Color.black)
+                .border(Color.black, width: 0.5)
+                .frame(alignment: .center)
+                .gesture(
+                    DragGesture(minimumDistance: 3, coordinateSpace: .local)
+                        .onEnded(
+                            {
+                                value in
+                                if value.translation.width < 0
+                                {
+                                    //swiped left
+                                    ImageName = SampleImages.IncrementSubSampleImageName(At: Key)
+                                    Updated.toggle()
+                                    ImageTitle = SampleImages.GetCurrentSubSampleImageTitle(At: Key)
+                                    print("ImageTitle=\(ImageTitle)")
+                                }
+                                if value.translation.width > 0
+                                {
+                                    //swiped right
+                                    ImageName = SampleImages.DecrementSubSampleImageName(At: Key)
+                                    Updated.toggle()
+                                    ImageTitle = SampleImages.GetCurrentSubSampleImageTitle(At: Key)
+                                    print("ImageTitle=\(ImageTitle)")
+                                }
+                            })
+                )
+        }
+    }
+}
+
+struct SubSampleImage_Preview: PreviewProvider
+{
+    @State static var NotUsed: String = ""
+    @State static var SampleName = "Sapporo2048x2014"
+    @State static var ImageTitle = "Sapporo"
+    
+    static var previews: some View
+    {
+        SubSampleImage(Key: .SubSampleScratchKey,
+                       UICommand: $NotUsed,
+                       ImageName: $SampleName,
+                       Updated: false,
+                       ImageTitle: $ImageTitle)
+    }
+}
