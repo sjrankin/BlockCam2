4c82f0023d8fc953f2a72054cfd77dc55d7318c2
Stuart Rankin <sjrankin@gmail.com>
2021-05-29 15:02:05 +0900

    Create BrightnessMaskParameters.swift
    Brightness mask parameters.

---
+++ BlockCam2/Filters/BuiltInFilters/MetalFilters/MaskingBlending/BrightnessMask/BrightnessMaskParameters.swift
@@ -0,0 +1,15 @@
+//
+//  BrightnessMaskParameters.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/27/21.
+//
+
+import Foundation
+import simd
+
+struct BrightnessMaskParameters
+{
+    let Invert: Bool
+    let Trigger: simd_float1
+}
