9bb439a9557e863dc71a01a521c9fc563996518b
Stuart Rankin <sjrankin@gmail.com>
2021-05-29 14:59:43 +0900

    Create TrailingFramesParameters.swift
    Parameters for multi-frame processing.

---
+++ BlockCam2/Filters/BuiltInFilters/MetalFilters/TrailingFrames/Kernel/TrailingFramesParameters.swift
@@ -0,0 +1,14 @@
+//
+//  TrailingFramesParameters.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/29/21.
+//
+
+import Foundation
+import simd
+
+struct TrailingFramesParameters
+{
+    let FrameCount: simd_uint1
+}
