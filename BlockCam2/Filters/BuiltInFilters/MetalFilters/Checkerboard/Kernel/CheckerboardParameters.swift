1e6137d798c20c821bb9677631ab830286b31d4d
Stuart Rankin <sjrankin@gmail.com>
2021-05-31 13:36:59 +0900

    Create CheckerboardParameters.swift
    Parameters for the metal checkerboard generator.

---
+++ BlockCam2/Filters/BuiltInFilters/MetalFilters/Checkerboard/Kernel/CheckerboardParameters.swift
@@ -0,0 +1,18 @@
+//
+//  CheckerboardParameters.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 5/31/21.
+//
+
+import Foundation
+import simd
+
+struct CheckerboardParameters
+{
+    let Q1Color: simd_float4
+    let Q2Color: simd_float4
+    let Q3Color: simd_float4
+    let Q4Color: simd_float4
+    let BlockSize: simd_float1
+}
