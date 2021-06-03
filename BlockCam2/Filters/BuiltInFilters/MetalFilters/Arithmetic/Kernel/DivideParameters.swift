43af235a5fa55f34bfbc15768923b9dc33c8a570
Stuart Rankin <sjrankin@gmail.com>
2021-06-04 08:27:00 +0900

    Create DivideParameters.swift
    Parameter block for the arithmetic division Metal kernel.

---
+++ BlockCam2/Filters/BuiltInFilters/MetalFilters/Arithmetic/Kernel/DivideParameters.swift
@@ -0,0 +1,14 @@
+//
+//  DivideParameters.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 6/3/21.
+//
+
+import Foundation
+import simd
+
+struct DivideParameters
+{
+    let Count: simd_float1
+}
