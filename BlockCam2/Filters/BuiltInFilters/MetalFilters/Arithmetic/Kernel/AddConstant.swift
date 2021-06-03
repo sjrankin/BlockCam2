d7d3b64635fed97474c01a80e62c1044d4567c9f
Stuart Rankin <sjrankin@gmail.com>
2021-06-04 08:27:55 +0900

    Create AddConstant.swift
    Metal kernel for adding a constant to channels in an image.

---
+++ BlockCam2/Filters/BuiltInFilters/MetalFilters/Arithmetic/Kernel/AddConstant.swift
@@ -0,0 +1,17 @@
+//
+//  AddConstant.swift
+//  BlockCam2
+//
+//  Created by Stuart Rankin on 6/3/21.
+//
+
+import Foundation
+import simd
+
+struct AddConstant
+{
+    let r: simd_float1
+    let g: simd_float1
+    let b: simd_float1
+    let NormalClamp: simd_bool
+}
