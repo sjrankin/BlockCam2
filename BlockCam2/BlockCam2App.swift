92832baa1e67e4896634cbe29dae7fcaf33cccca
Stuart Rankin <sjrankin@gmail.com>
2021-07-12 17:15:55 -0700

    Update BlockCam2App.swift
    Added code to detect dirty shut downs of previous instances to to reset the filter and filter group when that happens.

--- BlockCam2/BlockCam2App.swift
+++ BlockCam2/BlockCam2App.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main struct BlockCam2App: App
 {
+    init()
+    {
+        Settings.Initialize()
+        if Settings.IsFalse(For: .ClosedCleanly)
+        {
+            Debug.Print("Previous instantiation did not close cleanly.")
+            Settings.SetString(.CurrentFilter, "")
+            Settings.SetString(.CurrentGroup, "")
+        }
+    }
+    
     var body: some Scene
     {
         WindowGroup
